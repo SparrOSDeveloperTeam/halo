a7ea0f71c3afea9df15f2ab63eb0f70051143ee9
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2018-09-06 17:31:31 -0500

    Create writeiso.bat

---
+++ writeiso.bat
@@ -0,0 +1,36 @@
+@echo off
+title Halo Portable Edition Maker - Write ISO to USB
+if not exist halopem.iso goto noiso
+echo.
+echo ==================
+echo   Halo PEM Setup
+echo ==================
+echo.
+echo What is the destination drive? Use This PC or Computer to check.
+echo.
+echo (E.g. F:)
+echo.
+set /p destflashdrive=Drive 
+7za e archive.zip -o%DestFlashDrive%\ *.*
+goto end
+:noiso
+echo.
+echo ==================
+echo   Halo PEM Setup
+echo ==================
+echo.
+echo It seems as though you have not yet created an ISO. Please create an ISO to use this utility.
+echo.
+pause
+exit
+:end
+cls
+echo.
+echo ==================
+echo   Halo PEM Setup
+echo ==================
+echo.
+echo Setup has completed.
+echo.
+pause
+exit
