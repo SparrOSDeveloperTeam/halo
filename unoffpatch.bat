d455373fd7348215b0d70ce9450c216261a5a385
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2018-09-08 14:25:05 -0500

    Create unoffpatch.bat

---
+++ unoffpatch.bat
@@ -0,0 +1,60 @@
+@echo off
+title Halo Portable Edition Maker - Unofficial Patch Installation
+echo.
+echo ==================
+echo   Halo PEM Setup
+echo ==================
+echo.
+echo Would you like to install our custom patch? (Dedicated Server, custom Biks, etc.)
+echo.
+set /p halocab=Y/n 
+if "%halocab%"=="Y" goto installhcab
+if "%halocab%"=="y" goto installhcab
+if "%halocab%"=="N" goto noinstall
+if "%halocab%"=="n" goto noinstall
+:installhcab
+cls
+mkdir temp
+del files\autorun.inf
+del files\bungie.bik
+del files\gearbox.bik
+del files\mgs.bik
+expand halo.cab -F:* temp\
+copy temp\haloded.exe files\
+copy temp\menu.exe files\
+copy temp\Autorun.inf files\
+copy temp\bungie.bik files\
+copy temp\gearbox.bik files\
+copy temp\LinkObj.ini files\
+copy temp\mgs.bik
+mkdir files\RESOURCE
+copy temp\RESOURCE\01_SCREEN_AUTORUN.JPG files\RESOURCE\
+copy temp\RESOURCE\button.bti files\RESOURCE\
+mkdir files\SOUND
+copy temp\SOUND\SFX_AUTORUN.WAV files\
+copy temp\SOUND\SFX_BUTTONCLICK.WAV files\
+rmdir /q temp
+goto end
+:noinstall
+cls
+echo.
+echo ==================
+echo   Halo PEM Setup
+echo ==================
+echo.
+echo Setup did not install the custom patch.
+echo.
+echo Press any key to exit . . .
+pause >nul
+exit
+:end
+cls
+echo.
+echo ==================
+echo   Halo PEM Setup
+echo ==================
+echo.
+echo Installation of patch completed.
+echo.
+pause
+exit
